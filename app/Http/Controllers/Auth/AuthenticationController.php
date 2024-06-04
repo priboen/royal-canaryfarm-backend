bded355ad256ebdb66afa2b9b9bf0dfef7473121
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-04 10:48:50 +0700

    initial commit

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Dotenv\Util\Regex;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $request->validated();
+
+        $userData = [
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ];
+
+        $user = User::create($userData);
+        $token = $user->createToken('royalcanary')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('royalcanary')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+}
